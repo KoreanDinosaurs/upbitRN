9ccbdc9955d593270056d8b7aa66e5436dae42db
kangdonghyun <make_w@naver.com>
2023-01-25 15:16:46 +0900

    first setting

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
-  api.cache(true);
+module.exports = function (api) {
+  api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-  };
-};
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extensions: ['.ts', '.tsx', '.jsx', '.js', '.json'],
+          alias: {
+            '@': './src',
+            '@screens': './src/screens',
+            '@components': './src/components',
+          },
+        },
+      ],
+    ],
+  }
+}
